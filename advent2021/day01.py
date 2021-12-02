ce4906476c1048f9c1eac321b833b1a358dcdf17
Jacopo Farina <jacopo.farina@flixbus.com>
2021-12-02 16:08:35 +0100

    Open the file once and pass it to the solution functions

--- advent2021/day01.py
+++ advent2021/day01.py
@@ -1,36 +1,36 @@
-def part_one():
+def part_one(fr):
     """Count how many lines are greater than the previous one."""
-    with open("input/day01.txt") as fr:
-        previous = None
-        increases = 0
-        for line in fr:
-            if line.strip() == "":
-                continue
-            line_val = int(line)
-            if previous is not None and line_val > previous:
-                increases += 1
-            previous = line_val
+    previous = None
+    increases = 0
+    for line in fr:
+        if line.strip() == "":
+            continue
+        line_val = int(line)
+        if previous is not None and line_val > previous:
+            increases += 1
+        previous = line_val
     return increases
 
 
-def part_two():
+def part_two(fr):
     """Count how many lines are greater than 4 lines before."""
 
-    with open("input/day01.txt") as fr:
-        a, b, c, d = None, None, None, None
-        increases = 0
-        for line in fr:
-            if line.strip() == "":
-                continue
-            cur_val = int(line)
-            a, b, c, d = b, c, d, cur_val
-            if a is None:
-                continue
-            if d > a:
-                increases += 1
-        return increases
+    a, b, c, d = None, None, None, None
+    increases = 0
+    for line in fr:
+        if line.strip() == "":
+            continue
+        cur_val = int(line)
+        a, b, c, d = b, c, d, cur_val
+        if a is None:
+            continue
+        if d > a:
+            increases += 1
+    return increases
 
 
 if __name__ == "__main__":
-    print(part_one())
-    print(part_two())
+    with open("input/day01.txt") as fr:
+        print(part_one(fr))
+        fr.seek(0)
+        print(part_two(fr))
