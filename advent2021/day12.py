0928c687f2b32022184079c7f215144f537fde4f
Jacopo Farina <jacopo.farina@flixbus.com>
2021-12-12 10:42:13 +0100

    Add solution for day 12 part 1

---
+++ advent2021/day12.py
@@ -0,0 +1,43 @@
+def part_one(edges):
+    solution = [['start']]
+    while True:
+        new_list = []
+        # calculate all continuations of solution
+        # and put them in new_list
+        for path in solution:
+            last_node = path[-1]
+            if last_node == 'end':
+                new_list.append(path)
+                continue
+            for candidate_next in edges[last_node]:
+                # if start, ignore it
+                if candidate_next == 'start':
+                    continue
+                # if lowercase and already there, ignore it
+                if candidate_next.islower() and candidate_next in path:
+                    continue
+                # otherwise, expand and keep it
+                new_list.append(path + [candidate_next])
+        # if new_list ha same size of solution, stop
+        if len(new_list) == len(solution):
+            break
+        solution = new_list
+    for sol in solution:
+        print(sol)
+    return len(solution)
+
+
+def part_two():
+    ...
+
+
+if __name__ == "__main__":
+    with open("input/day12.txt") as fr:
+        edges = {}
+        for line in fr:
+            s, _, e = line.strip().partition('-')
+            edges[s] = edges.get(s, []) + [e]
+            edges[e] = edges.get(e, []) + [s]
+
+    print(part_one(edges))
+    print(part_two())
