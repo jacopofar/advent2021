5051790a9e622c1f04133439104eff9c214dd4bb
Jacopo Farina <jacopo.farina@flixbus.com>
2021-12-16 09:03:02 +0100

    Day 15 fixed

--- advent2021/day15.py
+++ advent2021/day15.py
@@ -1,31 +1,24 @@
+from queue import PriorityQueue
+
+
 def part_one(cells):
+    """Find shortest path cost using Dijkstra"""
     max_x = max(x for x, _ in cells)
     max_y = max(y for _, y in cells)
     total_risk = dict()
-    edge = {(0, 0): 0}
-    while True:
-        new_edge = dict()
-        # for each cell in edge, get the neighbors
-        for (x, y), risk_so_far in edge.items():
-            # add edge to the total_risk
-            total_risk[(x, y)] = risk_so_far
-            for (nx, ny) in [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]:
-                # for each neighbor not already explored
-                if (nx, ny) in cells and (nx, ny) not in total_risk:
-                    # calculate the risk of that neighbor, add it to new_edge
-                    if (nx, ny) in new_edge:
-                        new_edge[(nx, ny)] = min(
-                            risk_so_far + cells[(nx, ny)],
-                            new_edge[(nx, ny)]
-                        )
-                    else:
-                        new_edge[(nx, ny)] = risk_so_far + cells[(nx, ny)]
-
-        # if new_edge is empty, break the cycle
-        if len(new_edge) == 0:
-            break
-        # set edge = new_edge
-        edge = new_edge
+    to_explore = PriorityQueue()
+    to_explore.put((0, (0, 0)))
+    while not to_explore.empty():
+        cost, (x, y) = to_explore.get()
+        # an element may be put multiple times in the queue
+        # duplicates are sub-optimal, ignore them
+        if (x, y) in total_risk:
+            continue
+        total_risk[(x, y)] = cost
+        for (nx, ny) in [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]:
+            # for each neighbor not already explored
+            if (nx, ny) in cells and (nx, ny) not in total_risk:
+                to_explore.put((cost + cells[(nx, ny)], (nx, ny)))
     return total_risk[(max_x, max_y)]
 
 
@@ -45,8 +38,9 @@ def expand_cells(original_cells):
 
 def part_two(original_cells):
     new_cells = expand_cells(original_cells)
-    for row in range(50):
-        print("".join([str(new_cells[(row, col)]) for col in range(50)]))
+    # for row in range(50):
+    #     print("".join([str(new_cells[(row, col)]) for col in range(50)]))
+
     return part_one(new_cells)
 
 
@@ -56,8 +50,6 @@ if __name__ == "__main__":
         for rowi, line in enumerate(fr):
             for coli, v in enumerate(line.strip()):
                 cells[(rowi, coli)] = int(v)
+
     print(part_one(cells))
-    raise NotImplementedError(
-        "Currently part 2 works on the sample input but not on my real input. There must be an error but I have no time today to troubleshoot :("
-    )
     print(part_two(cells))
