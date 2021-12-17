8726d3896715def642d3d62facd92575ef3776df
Jacopo Farina <jacopo.farina@flixbus.com>
2021-12-17 18:46:50 +0100

    Add some comments to day 17

--- advent2021/day17.py
+++ advent2021/day17.py
@@ -17,9 +17,11 @@ def positions(speed_x, speed_y):
 
 @lru_cache
 def trajectories_and_max_heights(x1, x2, y1, y2):
+    # rough guess of max speed that makes sense
     max_speed = max(x1, x2, -y1)
     max_y_reached = 0
     valid_speeds = 0
+    # negative x is never a solution, y can be
     for sx in range(max_speed + 1):
         for sy in range(2 * (max_speed + 1)):
             sy -= max_speed + 1
@@ -27,6 +29,7 @@ def trajectories_and_max_heights(x1, x2, y1, y2):
             for x, y in positions(sx, sy):
                 max_y_in_this = max(y, max_y_in_this)
                 if x1 <= x <= x2 and y1 <= y <= y2:
+                    # hit
                     max_y_reached = max(max_y_reached, max_y_in_this)
                     valid_speeds += 1
                     break
